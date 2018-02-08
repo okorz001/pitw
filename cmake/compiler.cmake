fde0f52ce8cdd5bfcf14c8ea2188ad89c0c385aa
Oscar Korz <okorz001@gmail.com>
2018-02-07 18:25:47 -0800

    wip

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -9,6 +9,7 @@ elseif(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
     # Command line warning D9025 : overriding '/W3' with '/W4'
     string(REGEX REPLACE " ?/W[0-9]" "" CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
     # warning C4100: '_i': unreferenced formal parameter
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /W4 /WX /wd4100")
+    # warning C4232: address of dllimport 'malloc' is not static
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /W4 /WX /wd4100 /wd4232")
 endif()
 message(STATUS "Using CMAKE_C_FLAGS: ${CMAKE_C_FLAGS}")
